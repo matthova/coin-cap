883a3763a5db131855634f8493204385f3e5ad2d
hovanem <matt.hova@autodesk.com>
2017-12-12 17:11:52 -0800

    rounding off the final investment

--- src/Coin.js
+++ src/Coin.js
@@ -5,7 +5,9 @@ const Coin = props => (
     <h1>{props.name}</h1>
     <div>Market cap: ${Number(props.market_cap_usd)}</div>
     <div>Percent of total cap: %{Number(props.market_cap_usd) / props.totalCash}</div>
-    <div>Amount to invest: ${Number(props.market_cap_usd) / props.totalCash * props.cash}</div>
+    <div>
+      Amount to invest: ${Number(Number(props.market_cap_usd) / props.totalCash * props.cash).toFixed(2)}
+    </div>
   </div>
 );
 
